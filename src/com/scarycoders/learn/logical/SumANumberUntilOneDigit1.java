6749ad9e99f6dfa952432afd578eb44c66feabf7
ruksadsiddiqui <v-rsiddiqui>
2018-06-23 20:03:05 +0530

    reverse string

--- src/com/scarycoders/learn/logical/SumANumberUntilOneDigit1.java
+++ src/com/scarycoders/learn/logical/SumANumberUntilOneDigit1.java
@@ -3,26 +3,48 @@ package com.scarycoders.learn.logical;
 import java.util.ArrayList;
 
 public class SumANumberUntilOneDigit1 {
-    public static Integer sumit(Integer number1){
+   /* public static Integer sumit(Integer number1){
         int sum=0;
         int number=number1;
         while(number>9){
 
-            if(number<10 && sum>10) {
+            *//*if(number<10 && sum>10) {
                 sum+=number;
                 number = sum;
-            }
+            }*//*
             sum+=number % 10;
             number/=10;
+
         }
 
       // sum+=number;
         return sum;
+    }*/
+
+
+    static int digSum1(int n ){
+       int sum=0;
+       while(n>0 || sum >9){
+           if(n==0){
+               n=sum;
+               sum=0;
+           }
+           sum+=n%10;
+           n/=10;
+       }
+       return sum;
+    }
+
+    static int digSum(int n)
+    {
+        if (n == 0)
+            return 0;
+        return (n % 9 == 0) ? 9 : (n % 9);
     }
 
     public static void main(String[] args) {
-        System.out.println(sumit(1234));
+        System.out.println(digSum1(28101991));
 
-        
+
     }
 }
