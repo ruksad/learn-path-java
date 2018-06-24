b5fb9134c5682ae0595874267e3e22f28e2ad174
ruksadsiddiqui <v-rsiddiqui>
2018-06-24 10:12:48 +0530

    find non repeated words from string

---
+++ src/com/scarycoders/learn/logical/string/FindStringNotRepeated.java
@@ -0,0 +1,28 @@
+package com.scarycoders.learn.logical.string;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+/*
+find non repeated words from string
+ */
+public class FindStringNotRepeated {
+    static List<String> findNonRepeatedWords(String str){
+        String[] chunkedArr = str.split("\\s");
+        List<String> strings = Arrays.asList(chunkedArr);
+        Map<String,Integer> lm=new LinkedHashMap();
+        strings.forEach(x->{
+            lm.put(x,lm.get(x)==null?1:lm.get(x)+1);
+        });
+        List<String> collect = lm.entrySet().stream().filter((k) -> k.getValue() == 1).map(x -> x.getKey()).collect(Collectors.toList());
+        return collect;
+    }
+
+    public static void main(String[] args) {
+        String str="I hate ruksad , ruksad I love plastic bags but are banned in maharastra i will sue you";
+        List<String> nonRepeatedWords = findNonRepeatedWords(str);
+        System.out.println(nonRepeatedWords);
+    }
+}
