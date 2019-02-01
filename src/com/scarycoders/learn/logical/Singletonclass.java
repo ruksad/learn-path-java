86c66e696462a1f9eba37bfd65298349b0d64df7
v-rsiddiqui <v-rsiddiqui@shutterfly.com>
2019-02-01 20:52:44 +0530

    SingleTon synchronised class

--- src/com/scarycoders/learn/logical/Singletonclass.java
+++ src/com/scarycoders/learn/logical/Singletonclass.java
@@ -1,4 +1,15 @@
 package com.scarycoders.learn.logical;
 
 public class Singletonclass {
+  private static volatile Singletonclass singletonclass=null;
+  public static Singletonclass getInstance(){
+    synchronized (Singletonclass.class){
+      if(singletonclass==null)
+      {
+        singletonclass=new Singletonclass();
+      }
+      return singletonclass;
+    }
+  }
+  private Singletonclass(){ }
 }
--- src/com/scarycoders/learn/logical/string/FindStringNotRepeated.java
+++ src/com/scarycoders/learn/logical/string/FindStringNotRepeated.java
@@ -6,20 +6,22 @@ import java.util.stream.Collectors;
 find non repeated words from string
  */
 public class FindStringNotRepeated {
-    static List<String> findNonRepeatedWords(String str){
-        String[] chunkedArr = str.split("\\s");
-        List<String> strings = Arrays.asList(chunkedArr);
-        Map<String,Integer> lm=new LinkedHashMap();
-        strings.forEach(x->{
-            lm.put(x,lm.get(x)==null?1:lm.get(x)+1);
-        });
-        List<String> collect = lm.entrySet().stream().filter((k) -> k.getValue() == 1).map(x -> x.getKey()).collect(Collectors.toList());
-        return collect;
-    }
 
-    public static void main(String[] args) {
-        String str="I hate ruksad , ruksad I love plastic bags but are banned in maharastra i will sue you";
-        List<String> nonRepeatedWords = findNonRepeatedWords(str);
-        System.out.println(nonRepeatedWords);
-    }
+  static List<String> findNonRepeatedWords(String str) {
+    String[] chunkedArr = str.split("\\s");
+    List<String> strings = Arrays.asList(chunkedArr);
+    Map<String, Integer> lm = new LinkedHashMap();
+    strings.forEach(x -> {
+      lm.put(x, lm.get(x) == null ? 1 : lm.get(x) + 1);
+    });
+    List<String> collect = lm.entrySet().stream().filter((k) -> k.getValue() == 1)
+        .map(x -> x.getKey()).collect(Collectors.toList());
+    return collect;
+  }
+
+  public static void main(String[] args) {
+    String str = "I hate ruksad , ruksad I love plastic bags but are banned in maharastra i will sue you";
+    List<String> nonRepeatedWords = findNonRepeatedWords(str);
+    System.out.println(nonRepeatedWords);
+  }
 }
